c9b2fc8929d0b3bf6b366c07232146ae0597eca0
tairitsu86 <113744248+tairitsu86@users.noreply.github.com>
2023-09-04 21:46:10 +0800

    Delete esp32/edge_device_template directory

--- esp32/edge_device_template/src/WiFiConnector.h
+++
@@ -1,9 +0,0 @@
-#include "WiFi.h"
-
-void wifiConnect(String WIFI_SSID,String WIFI_PASSWORD){
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print(F("Try to connect to "));
-  Serial.print(WIFI_SSID);
-}
-
-
